199534695632be2289bf633ab27a56144b1e41d6
bhanu <bpst.web@gmail.com>
2022-08-31 14:48:16 +0530

    leaf disease prediction

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -2,24 +2,28 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
+import os.path
 
 from apps.home import blueprint
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+
+from werkzeug.utils import secure_filename
+import tensorflow as tf
+import numpy as np
+import imageio as iio
 
 
 @blueprint.route('/index')
 @login_required
 def index():
-
     return render_template('home/landing-freelancer.html', segment='index')
 
 
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
-
     try:
 
         if not template.endswith('.html'):
@@ -40,7 +44,6 @@ def route_template(template):
 
 # Helper - Extract current page name from request
 def get_segment(request):
-
     try:
 
         segment = request.path.split('/')[-1]
@@ -52,3 +55,29 @@ def get_segment(request):
 
     except:
         return None
+
+
+@blueprint.route('/get-predictions', methods=['POST'])
+def get_predictions():
+    global label
+    classes = ['angular_leaf_spot', 'bean_rust', 'healthy']
+
+    if not os.path.exists('uploads'):
+        os.makedirs('uploads')
+
+    if request.method == 'POST':
+        image = request.files['img']
+        image_name = secure_filename(image.filename)
+        image.save(os.path.join('uploads', image_name))
+
+        model = tf.keras.models.load_model('models/')
+        label = np.argmax(model.predict(decode_img(iio.imread(f'uploads/{image_name}'))), axis=1)
+
+    return render_template('home/leaf-disease-prediction-results.html',
+                           image_label=' '.join(classes[label[0]].split('_')).title())
+
+
+def decode_img(image):
+    img = tf.image.resize(image, [224, 224])
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    return np.expand_dims(img, axis=0)
